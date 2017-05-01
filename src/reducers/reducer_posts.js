7d268c50e3aecee3037d9ad67ce5da72b0d1efd7
Dennis Hedlund <denhed@Denniss-iMac.local>
2017-05-01 11:27:10 +0200

    show post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST, CREATE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
@@ -14,3 +16,12 @@ export default function(state = {}, action) {
 // { 4: post, 6: post}
 
 // lodash takes a object in a array and create a new object med selected property as a key.
+
+
+// fetch post es5 way
+/*
+const post = action.payload.data;
+const newState = { ...state };
+newState[post.id] = post;
+return newState;
+*/
