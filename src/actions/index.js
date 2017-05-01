d69df4d41c1f263aa0c4c5810046a3ab8f073f06
Dennis Hedlund <denhed@Denniss-iMac.zyxel.com>
2017-05-01 11:52:29 +0200

    delete post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://localhost:3000';
 
@@ -28,5 +29,14 @@ export function fetchPost(id) {
   return {
     type: FETCH_POST,
     payload: request
+  }
+}
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}`)
+  .then(() => callback());
+  return {
+    type: DELETE_POST,
+    payload: id
   }
 }
